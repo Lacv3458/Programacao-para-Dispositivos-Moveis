0dfcbc4662961c256e9b25204853a71805372bee
Luan Alejandro Cardoso Vazquez Gil <104587717+Lacv3458@users.noreply.github.com>
2023-03-10 15:57:59 -0300

    Update App.js

--- multiplicador_numeros/App.js
+++ multiplicador_numeros/App.js
@@ -1,64 +1,67 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, TextInput, Button} from 'react-native';
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
 
-
-class App extends Component{
-  constructor(props){
+class App extends Component {
+  constructor(props) {
     super(props);
     this.state = {
       n1: '',
       n2: '',
-      input: ''
+      input: '',
     };
-    
+
     this.entrar = this.entrar.bind(this);
   }
 
-
-  entrar(){
-    if (this.state.n1 === '' && this.state.n2 === ''){
-      alert('Digite dois numeros!')
+  entrar() {
+    if (this.state.n1 === '' || this.state.n2 === '') {
+      alert('Digite dois numeros!');
       return;
     }
-    this.setState({nome: 'Resultado: '+ this.state.n1 * this.state.n2});
+    else if((this.state.n1 >= 0 || this.state.n1 <= 0) && (this.state.n2 >= 0 || this.state.n2 <= 0)){
+      this.setState({ nome: 'Resultado: ' + this.state.n1 * this.state.n2 });
+    }
+    else{
+      alert('Digite apenas números!');
+      return;
+    }
+    
   }
 
-
-  render(){
-    return(
+  render() {
+    return (
       <View style={styles.area}>
-      <Text style={styles.texto}> Multiplicador de Números </Text>
+        <Text style={styles.texto}> Multiplicador de Números </Text>
 
-      <TextInput 
-      style={styles.input}
-      keyboardType = 'number-pad'
-      placeholder="Digite o primeiro n°"
-      onChangeText={ (int) => this.setState({n1: int})}
-      value = {this.state.myNumber}
-      />
+        <TextInput
+          style={styles.input}
+          keyboardType="number-pad"
+          placeholder="Digite o primeiro n°"
+          onChangeText={(valor) => this.setState({ n1: valor })}
+          value={this.state.myNumber}
+        />
 
-      <TextInput type="number"
-      style={styles.input}
-      keyboardType = 'number-pad'
-      placeholder="Digite o segundo n°"
-      onChangeText={ (int) => this.setState({n2: int})}
-      />
+        <TextInput
+          type="number"
+          style={styles.input}
+          keyboardType="number-pad"
+          placeholder="Digite o segundo n°"
+          onChangeText={(valor) => this.setState({ n2: valor })}
+        />
 
-      <Button title="Calcular" onPress={this.entrar} />
+        <Button title="Calcular" onPress={this.entrar} />
 
-
-      <Text style={styles.texto}> {this.state.nome} </Text>
+        <Text style={styles.texto}> {this.state.nome} </Text>
       </View>
     );
   }
 }
 
-
 const styles = StyleSheet.create({
-  container:{
+  container: {
     flex: 1,
   },
-  input:{
+  input: {
     height: 45,
     borderWidth: 1,
     borderColor: '#222',
@@ -66,11 +69,10 @@ const styles = StyleSheet.create({
     fontSize: 20,
     padding: 10,
   },
-  texto:{
+  texto: {
     textAlign: 'center',
     fontSize: 25,
-  }
-})
-
+  },
+});
 
 export default App;
